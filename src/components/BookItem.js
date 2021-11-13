69b8d09ea0551eafd6810e4f4f50905bc13fbd73
Meltrust <original.mtapia@outlook.com>
2021-11-13 16:49:22 -0600

    Add redux store, implement add book, validate inputs

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const BookItem = ({ title, author }) => (
+const BookItem = ({ title, author, category }) => (
   <div className="card pe-5">
     <div className="row mx-0 py-4 justify-content-between align-items-center ps-4 pe-5">
       <div className="col-md-6">
-        <p className="fw-bold opacity-50 mb-0">Fantasy</p>
+        <p className="fw-bold opacity-50 mb-0">{category}</p>
         <p className="fw-bold robotoslab fs-2 mb-0">{title}</p>
         <button type="button" className="link-primary border-0 bg-white p-0 mb-3">
           {author}
@@ -51,6 +51,7 @@ const BookItem = ({ title, author }) => (
 BookItem.propTypes = {
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
 };
 
 export default BookItem;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,43 +1,95 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/books';
 
-const Form = () => (
-  <div className="pb-5 pb-md-0">
-    <h2 className="h4 fw-bold text-muted">ADD NEW BOOK</h2>
-    <form className="row mx-0 g-0 justify-content-between pb-5 pb-md-0">
-      <div className="col-md-3">
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Book title"
-          required
-        />
-      </div>
-      <div className="col-md-3">
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Book author"
-          required
-        />
-      </div>
-      <div className="col-md-3">
-        <select
-          className="col-3 form-select try"
+const Form = () => {
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [category, setCategory] = useState('Fantasy');
 
-        >
+  const resetFields = () => {
+    setAuthor('');
+    setTitle('');
+    setCategory('Fantasy');
+  };
 
-          <option>Fantasy</option>
-          <option>Science Fiction</option>
+  const validate = (book) => {
+    if (title === '' || author === '') {
+      const errorPlace = document.getElementById('errPlace');
+      const errorMsg = document.createElement('small');
+      errorMsg.textContent = 'All fields are required';
+      errorMsg.classList.add('text-black-50', 'mt-3', 'error');
 
-        </select>
-      </div>
-      <div className="col-md-2 pb-5 pb-md-0">
-        <button type="submit" className="btn btn-primary w-100">
-          <span className="font-robotoslab small px-3">ADD BOOK</span>
-        </button>
-      </div>
-    </form>
-  </div>
-);
+      if (document.querySelector('.error') === null) {
+        errorPlace.append(errorMsg);
+      }
+    } else {
+      dispatch(addBook(book));
+      resetFields();
+      if (document.querySelector('.error') !== null) {
+        document.querySelector('.error').remove();
+      }
+    }
+  };
 
+  const submitBookToStore = (event) => {
+    event.preventDefault();
+    const newBook = {
+      id: uuidv4(), // make sure it's unique
+      title,
+      author,
+      category,
+    };
+    event.preventDefault();
+    validate(newBook);
+  };
+
+  return (
+    <div className="pb-5 pb-md-0">
+      <h2 className="h4 fw-bold text-muted"> ADD NEW BOOK</h2>
+      <form className="row mx-0 g-0 justify-content-between pb-5 pb-md-0" id="">
+        <div className="col-md-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Book title"
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+            required
+          />
+        </div>
+        <div className="col-md-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Book author"
+            value={author}
+            onChange={(event) => setAuthor(event.target.value)}
+            required
+          />
+        </div>
+        <div className="col-md-3">
+          <select
+            className="col-3 form-select try "
+            value={category}
+            onChange={(event) => setCategory(event.target.value)}
+          >
+
+            <option>Fantasy</option>
+            <option>Science Fiction</option>
+
+          </select>
+        </div>
+        <div className="col-md-2 pb-5 pb-md-0">
+          <button type="submit" className="btn btn-primary w-100" onClick={submitBookToStore}>
+            <span className="font-robotoslab small px-3">ADD BOOK</span>
+          </button>
+        </div>
+      </form>
+      <div id="errPlace" />
+    </div>
+  );
+};
 export default Form;
